53ff44e293b4bb868002de6bbdf88d791cae9d3a
rahelerahimi <rahimirahele16@gmail.com>
2023-12-27 17:28:03 +0330

    start-about-ecomment

---
+++ js/javaScript.js
@@ -0,0 +1,64 @@
+var mybutton = document.getElementById('myBtn')
+window.onscroll = function () {
+  scrollFunction()
+}
+function scrollFunction () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block'
+  } else {
+    mybutton.style.display = 'none'
+  }
+}
+function topFunction () {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+
+//  ---------------------------------to top--------------------------------------------
+document.addEventListener('DOMContentLoaded', function () {
+  animateCounter('customersCounter', 80)
+  animateCounter('projectsCounter', 200)
+  animateCounter('partnersCounter', 130)
+  animateCounter('currentProjectsCounter', 90)
+})
+
+function animateCounter (elementId, targetValue) {
+  let currentValue = 0
+  const counterElement = document.getElementById(elementId)
+
+  const intervalId = setInterval(function () {
+    if (currentValue < targetValue) {
+      currentValue++
+      counterElement.textContent = currentValue
+    } else {
+      clearInterval(intervalId)
+    }
+  }, 10)
+}
+
+//  ---------------------------------counter--------------------------------------------
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 10,
+  autoplay: {
+    delay: 5000
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
+})
+//  ---------------------------------carousel swiper-------------------------------------------
